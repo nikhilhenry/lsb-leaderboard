401d939a1f8123aef478238a3bef01b7128a88fc
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-11-28 20:12:40 +0530

    create leaderboard component

---
+++ src/components/LeaderBoard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>test</p>
+    <svg width="500" height="400" id="chart">
+      <g v-for="(value,i) in houses" :key="value.index" :transform="`translate(0,${i *40})`">
+        <rect height="30" :width="value * 10"></rect>
+        <text y="20">{{value}}</text>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'LeaderBoard',
+  data(){
+    return{
+      houses:[30,60,90,120]
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+  rect{
+    fill:rgb(29, 148, 184);
+  }
+
+  text{
+    fill:white;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home">
+    <h1>Welcome to the Legacy School House Leaderboard</h1>
+    <Leaderboard/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Leaderboard from '@/components/LeaderBoard'
 export default {
   name: 'Home',
   components: {
+    Leaderboard
   }
 }
 </script>
