2eb2774d328bf0161f272e392ccac98c65d4e069
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-11-29 19:54:38 +0530

    create update wrapper

---
+++ src/components/Updates.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Update v-for="(update,index) in updates" :key="index" :update="update"/>
+  </div>
+</template>
+
+<script>
+import Update from '@/components/Update.vue'
+export default {
+  name:'Updates',
+  components:{
+    Update
+  },
+  data(){
+    return{
+      updates:[{title:'Lorem Ipsum',message:'Lorem Ipsum dolor',points:10},{title:'Lorem Ipsum',message:'Lorem Ipsum dolor',points:10},{title:'Lorem Ipsum',message:'Lorem Ipsum dolor',points:10}]
+    }
+  }
+}
+</script>
