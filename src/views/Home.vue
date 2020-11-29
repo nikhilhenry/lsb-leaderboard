2e79df6675626e47d5968140bc304365084e418c
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-11-29 19:54:49 +0530

    add updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,6 @@
   <div class="home">
     <h1 class="title has-text-centered mt-6">Legacy School House Leaderboard</h1>
     <div class="section">
-      <div class="container">
         <div class="container">
           <div class="columns is-centered">
             <div class="column is-half">
@@ -10,18 +9,26 @@
             </div>
           </div>
         </div>
+        <div class="container">
+          <div class="columns is-centered">
+            <div class="column is-half">
+              <Updates/>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Leaderboard from '@/components/LeaderBoard'
+import Updates from '@/components/Updates'
 export default {
   name: 'Home',
   components: {
-    Leaderboard
+    Leaderboard,
+    Updates
   }
 }
 </script>
