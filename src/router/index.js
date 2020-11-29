11638e12c4095e89c40c3d5dc43a37e3999f2056
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-11-29 21:35:38 +0530

    add nav gaurds

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import firebase from 'firebase'
 
 Vue.use(VueRouter)
 
@@ -16,23 +17,23 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
   },
   {
     path: '/login',
     name: 'Login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+    meta:{
+      requiresGuest:true
+    },
+    component: () => import('../views/Login.vue')
   },
   {
     path: '/update_points',
     name: 'UpdatePoints',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/UpdatePoints.vue')
+    meta:{
+      requiresAuth:true
+    },
+    component: () => import('../views/UpdatePoints.vue')
   }
 ]
 
@@ -40,5 +41,40 @@ const router = new VueRouter({
   routes,
   mode:'history'
 })
+
+//Nav guards
+router.beforeEach((to,from,next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)){
+    //Check if No logged user
+    if(!firebase.auth().currentUser){
+      next({
+        path:'/login',
+        query:{
+          redirect: to.fullPath
+        }
+      });
+    }
+    else{
+      next();
+    }
+  }
+  else if(to.matched.some(record => record.meta.requiresGuest)){
+    if(firebase.auth().currentUser){
+      next({
+        path:'/',
+        query:{
+          redirect: to.fullPath
+        }
+      });
+    }
+    else{
+      next();
+    }
+  }
+  else{
+    next();
+  }
+})
+
 
 export default router
