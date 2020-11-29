007a1671e092734503eb9bc03cf1706af019011e
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-11-29 21:15:46 +0530

    add styles

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,11 +5,16 @@
       <article class="card">
         <div class="card-content">
           <h1 class="title has-text-centered">Login</h1>
+          <article class="message" v-if="errors" :class="IsSuccess ? 'is-success':'is-danger'">
+            <div class="message-body">
+              <p v-for="(error,index) in errors" :key="index">{{error.message}}</p>
+            </div>
+          </article>
           <div class="form">
             <div class="field">
               <label class="label">Email</label>
               <div class="control">
-                <input class="input" type="email" placeholder="example@lsb.edu.in" v-model="email" required>
+                <input class="input" type="email" placeholder="example@lsb-leaderboard.com" v-model="email" required>
               </div>
             </div>
             <div class="field">
@@ -32,8 +37,41 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+
 export default {
-  name:'Login'
+  name:'Login',
+  data(){
+    return{
+      email:null,
+      password:null,
+      errors:null,
+      IsSuccess:false
+    }
+  },
+  methods:{
+    login:function(e){
+      if(this.email && this.password){
+        firebase.auth()
+          .signInWithEmailAndPassword(this.email,this.password)
+          .then(
+            ()=>{
+              this.errors=[]
+              this.errors.push({message:'Loggedin Successfully'})
+              this.IsSuccess=true
+            },
+            err=>{
+              this.errors=[]
+              this.errors.push(err)
+              this.IsSuccess=false
+            }
+        )}
+        else {
+          alert("Sorry but you're not welcome")
+        }
+      e.preventDefault();
+    }
+  }
 }
 </script>
 
