88583a337dfea674ac4ba44f0442a353122f25f2
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-11-29 21:30:43 +0530

    create UpdatePoints

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
+  {
+    path: '/update_points',
+    name: 'UpdatePoints',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/UpdatePoints.vue')
   }
 ]
 
