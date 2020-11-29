7ae51dbda62becbc65be84ba32ba2a2e1faee840
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-11-29 20:32:38 +0530

    create login component

--- src/router/index.js
+++ src/router/index.js
@@ -17,11 +17,20 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode:'history'
 })
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="login">
+  <div class="columns is-mobile is-centered mt-6">
+    <div class="column is-one-quarter">
+      <article class="card">
+        <div class="card-content">
+          <h1 class="title has-text-centered">Login</h1>
+          <div class="form">
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="control">
+                <input class="input" type="email" placeholder="example@lsb.edu.in" v-model="email" required>
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Password</label>
+              <div class="control">
+                <input class="input" type="password" placeholder="password" v-model="password" required>
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <button class="button is-primary is-medium is-fullwidth mt-6" v-on:click="login">LOGIN</button>
+              </div>
+            </div>                     
+          </div>
+        </div>
+      </article>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name:'Login'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
