d1513f67212dfafd3a4c9e19313b8d327eba1f3d
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-11-30 08:04:40 +0530

    add points

---
+++ src/api/addMessage.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase'
+
+export async function addMessage(message){
+
+  console.log(message)
+
+  //create house document ref
+  const houseRef = firebase.firestore().collection('houses').doc(message.house);
+
+  //get house document
+  const doc = await houseRef.get();
+  if (!doc.exists) {
+    console.log('No such document!');
+  } else {
+    console.log('Document data:', doc.data());    
+    //update points
+    await houseRef.update({points:doc.data().points+Number(message.points)})
+
+    await firebase.firestore().collection('messages').add(message)
+
+
+    return {message:'Points updated sucessfully',success:true}
+
+    //add message
+  }
+
+}
