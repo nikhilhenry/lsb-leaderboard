86d5ee672044227d7432d312d0d05bde4383960f
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-12-11 23:47:25 +0530

    remove message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,13 +12,6 @@
         <div class="container">
           <div class="columns is-centered">
             <div class="column is-half">
-              <article class="message is-success">
-                <div class="message-body">
-                  <p>Please note that these scores are only temporary and live points will be updated from the date of event commencement (14/12/2020).</p>
-                  <p>Looking forward to enthusiastic participation!</p>
-                  <i>- Nikhil Henry, Student Council President.</i>
-                </div>
-              </article>
               <Updates/>
             </div>
           </div>
