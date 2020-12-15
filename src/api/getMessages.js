d0de69529196ce4d478e327c4e5b71edbd1ce680
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-12-15 09:31:01 +0530

    limit function

--- src/api/getMessages.js
+++ src/api/getMessages.js
@@ -3,9 +3,13 @@ import firebase from 'firebase'
 export async function getMessages(){
 
   //message collection ref
-  const messageRef = firebase.firestore().collection('messages').orderBy('created','desc')
+  const messageRef = firebase.firestore().collection('messages').where('mode', '==', 'live').orderBy('created','desc')
 
   //get messages
-  const snapshot = await messageRef.where('mode', '==', 'live').get();
+  const snapshot = await messageRef.limit(6).get();
   return snapshot
+}
+
+export async function getMoreMessages(){
+
 }
