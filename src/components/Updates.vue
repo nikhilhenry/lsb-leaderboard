d9f97c313103499d07ade8884d273ce6cef75a88
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-12-15 09:57:27 +0530

    get more messages

--- src/components/Updates.vue
+++ src/components/Updates.vue
@@ -4,13 +4,15 @@
       :data-key="'id'"
       :data-sources="updates"
       :data-component="updateComponent"
+      :estimate-size="6"
+      v-on:tobottom="onScrollToBottom"
     />
   </div>
 </template>
 
 <script>
 import Update from '@/components/Update.vue'
-import {getMessages} from '../api/getMessages'
+import {getMessages,getMoreMessages} from '../api/getMessages'
 import VirtualList from 'vue-virtual-scroll-list'
 
 export default {
@@ -21,6 +23,7 @@ export default {
   data(){
     return{
       updates:[],
+      lastDoc:Object,
       updateComponent:Update
     }
   },
@@ -31,8 +34,22 @@ export default {
         const update = doc.data()
         update.id = doc.id
         this.updates.push(update)
+        this.lastDoc = doc
       })
       })
+  },
+  methods:{
+    onScrollToBottom:async function(){
+      // this.updates = this.updates.concat(getMoreMessages(this.updates[this.updates.length-1]))
+      const updates = await getMoreMessages(this.lastDoc)
+      updates.forEach(doc=>{
+        const update = doc.data()
+        update.id = doc.id
+      
+        this.updates.push(update)
+        this.lastDoc = doc
+      })
+    }
   }
 }
 </script>
